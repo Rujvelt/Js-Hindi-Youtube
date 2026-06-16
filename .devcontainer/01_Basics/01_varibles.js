a79428f83a7a0258bc95878e816136e76deecd01
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-06-16 18:42:48 +0000

    datatypes

---
+++ .devcontainer/01_Basics/01_varibles.js
@@ -0,0 +1,16 @@
+const accountID = 123216
+let accountEmail = "Rujvelt@google.com"
+var accountPassword = "4578"
+accountCity = "nagpur"
+let accountstate;
+// accountID = 465654
+/*
+prefer not to use the var becasuse of issue in block and functional scope 
+*/
+
+accountEmail = "RR@434rr.com"
+accountPassword = "987654"
+accountCity = "pune"
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountstate]);
---
+++ .devcontainer/01_Basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";  /* Treat all code as a new java script version*/
+alert (3+3)    //we are using node js not browser
+console.log(3+3);
+console.log("Hitesh");
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);   // object
+console.log(typeof "string");
+
+ 
+
+
+ 
