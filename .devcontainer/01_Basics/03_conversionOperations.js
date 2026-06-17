6f7d20c3c9d077509accbc05fb065108306de35f
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-06-17 05:59:13 +0000

    conversion of datatypes

---
+++ .devcontainer/01_Basics/03_conversionOperations.js
@@ -0,0 +1,30 @@
+//let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+//let ValueInNumber = Number(score)
+
+// let isLoggedIn = undefined
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let huehue = 90
+// let lala = String(huehue)
+// console.log(lala);
+// console.log( typeof lala);
+
+// let isbabool = 0
+// let nagarjun = Boolean(isbabool)
+// console.log(nagarjun);
+// console.log( typeof nagarjun);
+
+let age = 45
+let realage = String(age)
+console.log(realage);
+console.log( typeof realage);
+
+
+
+
