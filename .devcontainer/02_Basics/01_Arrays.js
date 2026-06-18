a6fd6894b1efa19f3cf1e1361f0dab0453f6d352
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-06-18 10:44:16 +0000

    commit

---
+++ .devcontainer/02_Basics/01_Arrays.js
@@ -0,0 +1,39 @@
+// array
+const myArr = [45,19,25,38,44,58]
+// const myheros = ["superman","shaktiman"]
+// const myarr2 = new Array(1,2,3,4)
+// console.log(myArr[4]);
+
+
+// myArr.push(90,76,45)
+// myArr.pop()
+// myArr.unshift(1000)
+// myArr.shift()
+// console.log(typeof myArr);
+// console.log(myArr.includes(75));
+// console.log(myArr.indexOf(44));
+
+// const newarr = myArr.join()
+// console.log(myArr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+
+///-------slice and splice---------///
+
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log("B ",myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ",myArr);
+console.log(myn2);
+
+
+
+
+
+
