b78c11fe0eb12a43dc628c3b48794f4be6a32396
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-06-18 09:17:25 +0000

    done

---
+++ .devcontainer/01_Basics/07_dates.js
@@ -0,0 +1,26 @@
+
+let mydate = new Date()
+
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+
+let mycreatedate = new Date(2023,6,23)
+// console.log(mycreatedate.toDateString());
+// console.log(mycreatedate.toLocaleString());
+
+
+let mytimestamp = Date.now()
+// console.log(mytimestamp);
+// console.log(mycreatedate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
