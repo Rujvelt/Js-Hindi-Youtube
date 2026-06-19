10a8169498a1a00dacd82f3d7da1eea3b59cdee8
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-06-19 04:46:44 +0000

    commit

---
+++ .devcontainer/02_Basics/02_Arrays.js
@@ -0,0 +1,27 @@
+const Marvel_heros =["Ironman","Thor","Wanda"]
+const Dc_heros = ["Superman","Wonder Women","Batman"]
+
+// Marvel_heros.push(Dc_heros)
+// console.log(Marvel_heros);
+// console.log(Marvel_heros[3][1]);
+// const allhereos = Marvel_heros.concat(Dc_heros)
+// console.log(allhereos);
+
+// const all_newheros =[...Marvel_heros, ...Dc_heros]
+// console.log(all_newheros);
+
+// const modiArrey = [1,2,3,[4,5],[6,7,8,[9,10,11]]]
+// const meloniArrey = modiArrey.flat(Infinity)
+// console.log(meloniArrey);
+
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Hitesh"));
+console.log(Array.from({name:"Hitesh"}));///intresting case
+
+let score1 = 100
+let score2 =200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
---
+++ .devcontainer/02_Basics/03_Objects.js
@@ -0,0 +1,35 @@
+// object literals
+
+const mySym = Symbol("Key1")
+const Juser = {
+               name :"Rujvelt",
+               age : 34,
+               location : "Nagpur",
+               [mySym] : "WTF",
+               Email : "Rujvelt@123gmail.com",
+               LogeedIn : false,
+               LastLoginDay : ["Monday","Friday"],
+               "full name" : "Hitesh Chaudhary"
+              }
+
+            //   console.log(Juser["Email"])
+            //   console.log(Juser["full name"])
+            //   console.log(Juser.Email)
+            //   console.log(typeof Juser.mySym)
+
+            //   Juser.Email= "Hitesh@alphabet.com"
+            //   Object.freeze(Juser)
+            //   Juser.Email = "Hitesh@microsoft.com"
+            //   console.log(Juser);
+                            
+            //   Juser.Email = "hitesh@chatgpt.com"
+            //Object.freeze(Juser)
+            //   Juser.Email = "Hitesh@microsoft.com"
+            //   console.log(Juser);
+              
+              Juser.greeting = function()
+              {
+                console.log("hello js user");
+              }
+              console.log(Juser.greeting);
+              
