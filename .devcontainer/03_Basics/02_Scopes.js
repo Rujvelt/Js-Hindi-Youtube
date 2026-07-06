6fc1a83621f07c3bc512404b217eead485764803
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-07-06 11:59:12 +0000

    commit

---
+++ .devcontainer/03_Basics/02_Scopes.js
@@ -0,0 +1,91 @@
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// const pie = 3.14
+// console.log(pie);
+// pie = 5
+// console.log(pie);
+
+
+// if (true)
+// {
+//     let a = 90
+//     const b = 43
+//     var c = 30 
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let b = 300
+
+// if(true)
+// {
+//     const a = 90
+//     let b = 900
+//     console.log("Inner value:", a);
+// }
+// console.log("Outer value:",b);
+
+
+// In this we have seen for an block scope and global scope so if we 
+// write declare an values inside the block and try to print it inside the blokc only this called as an block scope 
+// and when we decare the values in the global level and then print it this called as an global scope
+
+
+
+
+
+
+
+
+
+
+
+// function one ()
+// {
+//     const username = "Rujvelt"
+
+//     function two ()
+//     {
+//         const website = "Youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     // two()
+// }
+// one()
+
+if(true)
+{
+    const username = "Rujvelt"
+    if(username === "Rujvelt")
+    {
+        const website =" Facebook"
+        //console.log(username+ website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+
+
+//++++++++++++Intresting++++++++++++++++
+
+
+function addone (num)
+{
+    return num +1
+}
+console.log(addone(5))
+
+const addtwo = function(num)
+{
+    return num +2
+}
+console.log(addtwo(5));
