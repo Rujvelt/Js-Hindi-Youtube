8f725285471914b0bf36955a65cde2d4757218ce
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-07-08 09:01:34 +0000

    commit

---
+++ .devcontainer/04_ControlFlow/one.js
@@ -0,0 +1,94 @@
+// const isuserloggedin = true
+
+// if(2=="2")
+// {
+//     console.log("executed");
+// }
+
+// const tempreature = 60
+
+// if(tempreature<50)
+// {
+//     console.log("less than 50");
+// }
+// else
+// {
+//     console.log("tempreature is greater than 50");
+// }
+// console.log("execute");
+
+
+// const score = 200
+// if(score >100)
+// {
+//     const power = "fly"
+//     console.log(`User Power${power}`);
+// }
+// console.log(`User Power${power}`);
+
+
+
+// const balance = 1000
+// if(balance>1000) console.log("Preety good amount u have");
+// we didnt write this kind of code in any company this is not an good practise 
+
+
+
+// const balance = 1000
+
+// if(balance < 500)
+// {
+//     console.log("Your balance is less than 500");
+// }
+//  else if(balance < 700)
+// {
+//     console.log("Your balance is less than 700");
+// }
+//  else if(balance < 900)
+// {
+//     console.log("Your balance is less than 900");
+// }
+// else
+// {
+//     console.log("less than 1200");
+// }
+
+
+
+
+
+const UserlogedIn = true 
+const DebitCard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if(UserlogedIn && DebitCard && 2==2)
+{
+    console.log("Allow the user to purchase the course");
+}
+else
+{
+    console.log("You are not allowed to buy an course")
+}
+
+
+if( LoggedInFromGoogle || LoggedInFromEmail || GuestUser)
+{
+    console.log("User Logged In");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ .devcontainer/04_ControlFlow/switch.js
@@ -0,0 +1,41 @@
+// switch(key)
+// {
+//     case value:
+//         break;
+//         default:
+//         break;
+// }
+
+
+
+
+
+
+
+
+const month = 2
+switch (month) {
+    case 1:
+        console.log("This is january month");
+        break;
+    case 2:
+        console.log("This is February month");
+        break;
+    case 3:
+        console.log("This is March month");
+        break;
+    case 4:
+        console.log("This is April month");
+        break;
+
+    default:
+        console.log("this month is not mentioned in the code");
+        break;
+}
+
+
+
+
+
+
+
