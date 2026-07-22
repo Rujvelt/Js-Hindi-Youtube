c8300c41d72db0b77f7e6359b96f330a045e3b31
Rujvelt Wasnik <140255619+Rujvelt@users.noreply.github.com>
2026-07-22 05:40:36 +0000

    commit

--- .devcontainer/04_ControlFlow/switch.js
+++ .devcontainer/04_ControlFlow/switch.js
@@ -13,25 +13,25 @@
 
 
 
-const month = 2
-switch (month) {
-    case 1:
-        console.log("This is january month");
-        break;
-    case 2:
-        console.log("This is February month");
-        break;
-    case 3:
-        console.log("This is March month");
-        break;
-    case 4:
-        console.log("This is April month");
-        break;
+// const month = "December"
+// switch (month) {
+//     case "january":
+//         console.log("This is january month");
+//         break;
+//     case "February":
+//         console.log("This is February month");
+//         break;
+//     case "March":
+//         console.log("This is March month");
+//         break;
+//     case "April":
+//         console.log("This is April month");
+//         break;
 
-    default:
-        console.log("this month is not mentioned in the code");
-        break;
-}
+//     default:
+//         console.log("this month is not mentioned in the code");
+//         break;
+// }
 
 
 
---
+++ .devcontainer/04_ControlFlow/truthy.js
@@ -0,0 +1,49 @@
+// this is for some value is something is inside this consider as true value
+// const usermail = "Rujvelt@LoggedInFromGoogle.com"
+
+// if(usermail)
+// {
+//     console.log("Got an useremail");
+// }
+// else
+// {
+//     console.log("Nahh dont have an usermail");
+// }
+
+// this is for empty stirng output is false value 
+// const useremail = ""
+// if(useremail)
+// {
+//     console.log("got an email");
+// }
+// else
+// {
+//     console.log("didnt got an mail");
+// }
+
+// const useremail = []
+// if(useremail)
+// {
+//     console.log("yes u can enter the page");
+// }
+// else
+// {
+//     console.log("u cant enter here");
+// }
+
+
+
+//falsy value what are they -->
+//  nan, null,undefined,0,-0,BigInt,0n,""
+
+
+// truthy values what are they--> 
+// "0", 'false'," ",[],{},fuction(){},
+
+
+
+const useremail = ""
+if(useremail.length === 0)
+{
+
+}
